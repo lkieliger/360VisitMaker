a1d6eab259b82f9445af840505a89e466526f95e
lkieliger <leandro.kieliger@epfl.ch>
2017-02-07 17:51:21 +0100

    Dummy tests

---
+++ test/graphtests/GraphTest.java
@@ -0,0 +1,16 @@
+package graphtests;
+
+import org.junit.Test;
+
+/**
+ * 360VisitMaker - LKieliger on 07.02.2017.
+ */
+
+
+public class GraphTest {
+
+    @Test
+    public void dummyTest() {
+
+    }
+}
---
+++ test/graphtests/NeighborTest.java
@@ -0,0 +1,16 @@
+package graphtests;
+
+import org.junit.Test;
+
+/**
+ * 360VisitMaker - LKieliger on 07.02.2017.
+ */
+
+
+public class NeighborTest {
+
+    @Test
+    public void dummyTest() {
+
+    }
+}
